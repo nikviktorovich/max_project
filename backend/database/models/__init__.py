3af36ad9b6b47fca48d89c121f29ab236c22fb56
lnfecteDru <nikitasmn98@gmail.com>
2023-02-17 19:06:03 +0300

    Added product models

--- backend/database/models/__init__.py
+++ backend/database/models/__init__.py
@@ -2,3 +2,5 @@ from .auth import User
 from .auth import Permission
 from .auth import Group
 from .media import Image
+from .product import Product
+from .product import ProductImage
---
+++ backend/database/models/product.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import List
+from sqlalchemy import func
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped
+from .auth import User
+from .media import Image
+from .. import Base
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str]
+    stock: Mapped[int]
+    price_rub: Mapped[float]
+    added: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
+    last_updated: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
+    owner_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    owner: Mapped['User'] = relationship()
+    images: Mapped[List['ProductImage']] = relationship(back_populates='product')
+
+
+class ProductImage(Base):
+    __tablename__ = 'productimages'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'))
+    product: Mapped['Product'] = relationship(back_populates='images')
+    image_id: Mapped[int] = mapped_column(ForeignKey('images.id'))
+    image: Mapped['Image'] = relationship()
