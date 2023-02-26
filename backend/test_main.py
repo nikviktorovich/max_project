5ed403b44b1655fdceb0a5d36d6db827a2c40657
lnfecteDru <nikitasmn98@gmail.com>
2023-02-26 10:43:31 +0300

    Added tests

--- backend/test_main.py
+++ backend/test_main.py
@@ -4,6 +4,7 @@ import sqlalchemy.ext.declarative
 import sqlalchemy.orm
 import database
 import main
+from fastapi import status
 from fastapi.testclient import TestClient
 
 
@@ -20,8 +21,6 @@ TestingSessionLocal = sqlalchemy.orm.sessionmaker(
     autoflush=False,
 )
 
-database.Base.metadata.create_all(bind=engine)
-
 
 def get_test_db():
     db = TestingSessionLocal()
@@ -36,5 +35,48 @@ main.app.dependency_overrides[main.get_db] = get_test_db
 client = TestClient(main.app)
 
 
-class TestSelect(unittest.TestCase):
-    pass
+class TestAPI(unittest.TestCase):
+    def setUp(self) -> None:
+        database.Base.metadata.create_all(bind=engine)
+    
+    def tearDown(self) -> None:
+        database.Base.metadata.drop_all(bind=engine)
+    
+    def test_permissions(self):
+        response = client.get('/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertFalse('token' in response.json())
+        
+        response = client.get('/products')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_registering(self):
+        # Needs to be authorized
+        response = client.get('/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+        # No data specified
+        response = client.post('/signup')
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+        
+        # Username and password are too short
+        data = {
+            'username': 'short',
+            'password': 'short'
+        }
+        response = client.post('/signup', data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+        # Successful registering
+        data = {
+            'username': 'username',
+            'password': 'password'
+        }
+        response = client.post('/signup', data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        token = response.json()
+        
+        # Successful calling index while being authorized
+        headers = {'Authorization': f'Bearer {token["access_token"]}'}
+        response = client.get('/', headers=headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
