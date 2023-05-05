dde9a554a32812bd01933d406ebf0a77a113f5ed
lnfecteDru <nikitasmn98@gmail.com>
2023-05-05 12:00:21 +0300

    Added orm mappers

---
+++ src/market/mappers.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import registry
+from sqlalchemy.orm import relationship
+
+from . import models
+from . import domain
+
+
+def start_mappers() -> None:
+    mapper_registry = registry()
+
+    mapper_registry.map_imperatively(domain.models.CartItem, models.CartItem)
+    mapper_registry.map_imperatively(domain.models.Image, models.Image)
+    mapper_registry.map_imperatively(domain.models.ProductImage, models.ProductImage)
+    mapper_registry.map_imperatively(domain.models.Product, models.Product)
+    mapper_registry.map_imperatively(domain.models.User, models.User)
