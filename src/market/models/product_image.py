44e675feb25804c8b8e8347ce08f8c7e5f389c51
lnfecteDru <nikitasmn98@gmail.com>
2023-05-05 12:02:09 +0300

    Removed unused product_image file in models

--- src/market/models/product_image.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import Mapped
-from .media import Image
-from .product import Product
-from ..database import Base
-
-
