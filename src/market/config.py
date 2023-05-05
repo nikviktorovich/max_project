203769bba1baa1a9ca43ff0ea428acb4ecf51901
lnfecteDru <nikitasmn98@gmail.com>
2023-05-05 12:01:15 +0300

    Added config file

---
+++ src/market/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+def get_hash_algorithm() -> str:
+    return os.environ['HASH_ALGORITHM']
+
+
+def get_hash_secret_key() -> str:
+    return os.environ['HASH_SECRET_KEY']
+
+
+def get_access_token_expire_minutes() -> int:
+    return int(os.environ['ACCESS_TOKEN_EXPIRE_MINUTES'])
